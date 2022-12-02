68e6e6a6c777db129963415f86295ff3bbcad8cc
gijsvn <69443796+gijsvn@users.noreply.github.com>
2022-12-02 11:16:54 +0100

    Add files via upload

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
+namespace sudoku
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+    public class SudokuBlok
+    {
+        int[,] blok = new int[3, 3];
+        int[,] mask = new int[3, 3];
+    }
+    public class Sudoku
+    {
+        SudokuBlok[,] field = new SudokuBlok[3, 3];
+    }
+}
