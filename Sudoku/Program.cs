98af1309fb0db7de672f2b681a703d216b433397
Cornee5674 <corneeruiter@gmail.com>
2022-12-11 13:37:48 +0100

    Runs now with txt file instead of cmd arguments. Check main for instructions.

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -13,7 +13,20 @@ namespace sudoku
             // Can copy paste these:
             // 0 0 3 0 2 0 6 0 0 9 0 0 3 0 5 0 0 1 0 0 1 8 0 6 4 0 0 0 0 8 1 0 2 9 0 0 7 0 0 0 0 0 0 0 8 0 0 6 7 0 8 2 0 0 0 0 2 6 0 9 5 0 0 8 0 0 2 0 3 0 0 9 0 0 5 0 1 0 3 0 0
             // You can also run the application from the command line and just fill in the 81 ints from there
-            Sudoku sudoku = new Sudoku(convertToInt(args));
+            //
+            // Alternatively, you can now copy and paste the 81 ints into the Arguments.txt file. Just make sure to right click this file, and
+            // choose quick properties, and select copy to output directory.
+            string text = File.ReadAllText("Arguments.txt");
+            string[] textArgs = text.Split(" ");
+            Sudoku sudoku;
+            if (args.Length > 0)
+            {
+                sudoku = new Sudoku(convertToInt(args));
+            }else
+            {
+                sudoku = new Sudoku(convertToInt(textArgs));
+            }
+
             Console.WriteLine(evaluation(sudoku));
         }
 
